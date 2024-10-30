5f9cec771d56a1fb257fe9662565805e06032598
xinyi.xie2@duke.edu <xx98@duke.edu>
2024-10-29 22:12:45 -0400

    fix: console output

--- backend/routes/route.ts
+++ backend/routes/route.ts
@@ -45,7 +45,6 @@ router.post("/login", async (req,res) => {
 });
 
 router.get("/:userId", async (req,res) => {
-    console.log("Get Profile request");
     const {userId} = req.params;
 
     const user = await mgsfunlib.get(userId);
