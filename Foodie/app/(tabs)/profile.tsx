89f39158c679d49703f575aa2f8d84106012e176
Jo Wang <jo.wang@duke.edu>
2024-10-29 20:47:50 -0400

    feat: add non-logged in display on profile page

--- Foodie/app/(tabs)/profile.tsx
+++ Foodie/app/(tabs)/profile.tsx
@@ -1,4 +1,12 @@
-import { View, Text, StyleSheet, SafeAreaView, Image, Pressable, GestureResponderEvent } from "react-native";
+import {
+  View,
+  Text,
+  StyleSheet,
+  SafeAreaView,
+  Image,
+  Pressable,
+  GestureResponderEvent,
+} from "react-native";
 import { useAppContext } from "@/context/contexts";
 import { Redirect, useRouter } from "expo-router";
 import { useEffect, useState } from "react";
@@ -16,10 +24,8 @@ export default function Tab() {
   const router = useRouter();
 
   // variables
-  const img_path = "../../assets/18.png";
-  const img_path1 = "../../assets/burger.png";
-  const img_path2 = "../../assets/rasberry.png";
-  const img_path3 = "../../assets/peanut.png";
+  const img_path = "../../assets/peanut.png";
+  const img_path1 = "../../assets/smile.png";
 
   // intake demo data
   const data = {
@@ -59,48 +65,74 @@ export default function Tab() {
     fetchData();
   }, [userInfo]);
 
+  // functions
   function onPressLoginOut(event: GestureResponderEvent): void {
     throw new Error("Function not implemented.");
   }
 
-  // functions
+  function onPressLogin(event: GestureResponderEvent): void {
+    console.log("user: press login btn");
+    router.push("/login");
+  }
+
+  function onPressSignUp(event: GestureResponderEvent): void {
+    console.log("user: press sign up btn");
+    router.push("/signup");
+  }
 
   return (
     <SafeAreaView style={styles.safearea}>
-      <View style={styles.container}>
-        <Image
-          source={{ uri: parseImage(userInfo?.icon) }}
-          style={styles.avatar}
-          resizeMode="contain"
-        />
-        <Text style={styles.title}>{userInfo?.username}</Text>
-        <View style={styles.shopping_list}>
-          <Text style={styles.title}>My Shopping List</Text>
-          <View style={styles.container_row}></View>
-        </View>
-        <View style={styles.shopping_list}>
-          <Text style={styles.title}>Calorie intake</Text>
-          <LineChart
-            data={data}
-            width={300}
-            height={100}
-            chartConfig={chartConfig}
+      {id ? (
+        <View style={styles.container}>
+          <Image
+            source={{ uri: parseImage(userInfo?.icon) }}
+            style={styles.avatar}
+            resizeMode="contain"
           />
-        </View>
-        <View style={styles.shopping_list}>
-          <Text style={styles.title}>My Food allergies</Text>
-          <View style={styles.container_row}>
-            <Image
-              source={require(img_path3)}
-              style={styles.img}
-              resizeMode="contain"
+          <Text style={styles.title}>{userInfo?.username}</Text>
+          <View style={styles.shopping_list}>
+            <Text style={styles.title}>My Shopping List</Text>
+            <View style={styles.container_row}></View>
+          </View>
+          <View style={styles.shopping_list}>
+            <Text style={styles.title}>Calorie intake</Text>
+            <LineChart
+              data={data}
+              width={300}
+              height={100}
+              chartConfig={chartConfig}
             />
           </View>
-        </View>
-        <Pressable style={styles.btn} onPress={onPressLoginOut}>
+          <View style={styles.shopping_list}>
+            <Text style={styles.title}>My Food allergies</Text>
+            <View style={styles.container_row}>
+              <Image
+                source={require(img_path)}
+                style={styles.img}
+                resizeMode="contain"
+              />
+            </View>
+          </View>
+          <Pressable style={styles.btn_logout} onPress={onPressLoginOut}>
             <Text style={styles.btn_text}>Log Out</Text>
           </Pressable>
-      </View>
+        </View>
+      ) : (
+        <View style={styles.container}>
+          <Image
+            source={require(img_path1)}
+            style={styles.avatar}
+            resizeMode="contain"
+          />
+          <Text style={styles.title}>Login or sign up to track your favorites and daily intake!</Text>
+          <Pressable style={styles.btn_login} onPress={onPressLogin}>
+            <Text style={styles.btn_login_text}>Login</Text>
+          </Pressable>
+          <Pressable style={styles.btn_signup} onPress={onPressSignUp}>
+            <Text style={styles.btn_signup_text}>Sign Up</Text>
+          </Pressable>
+        </View>
+      )}
     </SafeAreaView>
   );
 }
@@ -126,6 +158,7 @@ const styles = StyleSheet.create({
     fontSize: 20,
     fontWeight: "bold",
     color: "black",
+    textAlign: "center"
   },
   avatar: {
     height: 80,
@@ -144,17 +177,44 @@ const styles = StyleSheet.create({
     width: "90%",
     padding: 20,
   },
-  btn: {
+  btn_logout: {
     height: 55,
     width: "90%",
     backgroundColor: "#042628",
     borderRadius: 16,
     justifyContent: "center",
     alignItems: "center",
-    marginBottom: 20
+    marginBottom: 20,
   },
   btn_text: {
     color: "#FFFFFF",
+    fontSize: 16,
+    fontWeight: "bold",
+  },
+  btn_login: {
+    height: 55,
+    width: "80%",
+    backgroundColor: "#042628",
+    borderRadius: 16,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  btn_login_text: {
+    color: "#FFFFFF",
+    fontSize: 16,
+    fontWeight: "bold",
+  },
+  btn_signup: {
+    height: 55,
+    width: "80%",
+    backgroundColor: "#FFFFFF",
+    borderRadius: 16,
+    justifyContent: "center",
+    alignItems: "center",
+    borderWidth: 1
+  },
+  btn_signup_text: {
+    color: "#042628",
     fontSize: 16,
     fontWeight: "bold",
   },
